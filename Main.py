c41f8dcad2df4f1d252f8ff9f5336775d2de678e
itsArno <arslan1muhammad11@gmail.com>
2023-08-19 00:56:32 +0500

    more methods added in main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,9 @@ import sys, os
 from Task import Task
 from tabulate import tabulate
 
-
+menues = {1: "Add Task", 2: "View Tasks", 3: "Remove Task", 4: "Exit"}
+available_menu = []
+current_menu = 1
 task = Task()
 styles = [
         "plain",
@@ -44,40 +46,131 @@ styles = [
         ]
 
 def main():
-    clear_screen()
-    display_title()
-    main_menu()
-    take_input()
+    show_menu()
+    
+def select_menu():
+    global current_menu
+    menu = input("Select: ")
+    if menu not in available_menu:
+        menu = "_"
+    match menu:
+        case "1":
+            current_menu = 1
+        case "2":
+            current_menu = 2
+        case "3":
+            current_menu = 3
+        case "4":
+            current_menu = 4
+        case _:
+            print("Invalid Input")
+            select_menu()
+    show_menu()
 
-def take_input():
-    input("Select: ")
+def app_title():
+    print(tabulate((["|     ToDo List      |"],)))
 
-def display_title():
-    print(tabulate(["ToDo List"]))
-
-def view_tasks(style_no=0):
-    styles
-    if len(sys.argv) > 1:
-        task.view(sys.argv[1])
+def view_tasks(style_no=22):
+    if task.count <= 0:
+        print("No Tasks")
     else:
-        index = bound_in_range(style_no)
-        task.view(style=styles[index])
+        if len(sys.argv) > 1:
+            task.view(sys.argv[1])
+        else:
+            index = bound_in_range(style_no)
+            task.view(style=styles[index])
         
 def bound_in_range(number):
     lower_bound = 0
     upper_bound = len(styles) - 1
     return max(min(number, upper_bound), lower_bound)
 
-def main_menu():
-    menu = [ [" 1-", "Add Task"], [" 2-", "View Tasks"] ]
-    print(f'\n{tabulate(menu, tablefmt="plain")}\n')
+def show_menu():
+    global current_menu
+    clear_screen()
+    app_title()
+    open_current_menu()
+    navi_menu(current_menu)
+    select_menu()
 
-def navi_menu():
-    menu = [["0-Back", "00-Main Menu"]]
-    print(tabulate(menu))
+def add_task():
+    global current_menu
+    print("(Note: Press 'ctr + c' to save)")
+    print("Add Task:")
+    print_previous_tasks()
+    exit = False
+    while not exit:
+        try:
+            task.add(input(f"{task.count + 1}: "))
+        except KeyboardInterrupt:
+            current_menu = 2
+            exit = True
+
+def print_previous_tasks():
+    if task.count > 0:
+        for i in range(task.count):
+            print(f"{i + 1}: {task.task[i][0]}")
+def open_current_menu():         
+    global current_menu           
+    match current_menu:
+        case 1:
+            add_task()
+            show_menu()
+        case 2:
+            view_tasks()
+        case 3:
+            remove_task()
+        case 4:
+            clear_screen()
+            sys.exit()
+        case _:
+            show_menu()
+
+def remove_task():
+    global current_menu
+    print("Remove Task (Note: Press 'ctr + c' to go back)")
+    view_tasks()
+    text = input("Select Task: ")
+    try: 
+        if text.isdigit():
+            remove_by_index(text)
+        else:
+            remove_by_name(text)
+        show_menu()
+    except:
+        current_menu = 2
+        show_menu()
+    
+
+def remove_by_name(text):
+    task.remove_by_name(text)
+
+def remove_by_index(text):
+    index = int(text)
+    count = task.count
+    if 0 < index <= count:
+        task.remove_by_index(index-1)
+    else:
+        print(f"index: {index}")
+        print("Selected Index is not in task list")
+        remove_task()
+
+def navi_menu(current_menu):
+    global available_menu
+    available_menu = []
+    nav_menu = ""
+    for menu in menues:
+        if menu is current_menu or (task.count <= 0 and menu is 3):
+            continue
+        nav_menu += (f"[{menu}-{menues[menu]}] ")
+        available_menu.append(menu)
+    print(tabulate(([nav_menu],)))
 
 def clear_screen():
-    os.system("cls")
+    if os.name == "posix": # POSIX systems (Linux, MacOS)
+        os.system("clear")
+    elif os.name == "nt":  # Windows
+        os.system("cls")
 
 if __name__ == "__main__":
     main()
--- Task.py
+++ Task.py
@@ -3,6 +3,7 @@ from tabulate import tabulate
 class Task:
     def __init__(self):
         self.task = []
+        self.count = len(self.task)
 
     def __str__(self):
         return f"task: {self.task}"
@@ -15,14 +16,28 @@ class Task:
     def task(self, task):
         self._task = task
 
-    def view(self, style="pretty"):
+    def view(self, style="grid"):
         headers = [ "No", "Task", "Status" ]
-        print(tabulate(self.task, headers=headers, showindex="always", tablefmt=style))
+        print(tabulate(self.task, headers=headers, tablefmt=style, showindex=range(1, self.count + 1)))
     
-    def remove(self):
-        ...
+    def remove_by_index(self, index):
+        del self.task[index]
+
+    def remove_by_name(self, name):
+        for i in range(len(self.task)):
+            if name in self.task[i]:
+                del self.task[i]
+
     #☐ 🗹
-    def add(self, task, status="☐"):
+    def add(self, task, status="Pending"):
         self.task.append([task, status])
 
+    @property
+    def count(self):
+        self._count = len(self.task)
+        return self._count
     
+    @count.setter
+    def count(self, count):
+        self._count = count
+        
