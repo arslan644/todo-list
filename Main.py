2724bf2ec68fa5fd3ae56b7e41b67a8ae89b9004
itsArno <arslan1muhammad11@gmail.com>
2023-08-20 04:48:03 +0500

    before making different class for menus

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ import sys, os, time
 from Task import Task
 from tabulate import tabulate
 
-menues = {1: "Add Task", 2: "View Tasks", 3: "Remove Task", 4: "Mark Task", 5: "Exit"}
 available_menu = []
 current_menu = 1
 task = Task()
@@ -45,12 +44,20 @@ styles = [
         "textile",
         "tsv"
         ]
+menues = {1: "Add Task", 2: "View Tasks", 3: "Remove Task", 4: "Mark Task", 5: "Exit"}
+
+def app_title():
+    print(tabulate((["|     ToDo List      |"],)))
 
 def main():
     show_menu()
 
-def testing():
-    print("checking")
+def show_menu():
+    clear_screen()
+    app_title()
+    open_current_menu()
+    navi_menu()
+    select_menu()
 
 def select_menu():
     global current_menu
@@ -63,8 +70,32 @@ def select_menu():
         current_menu = int(menu)
         show_menu()
     
-def app_title():
-    print(tabulate((["|     ToDo List      |"],)))
+def open_current_menu():         
+    global current_menu           
+    match current_menu:
+        case 1:
+            add_task()
+        case 2:
+            view_tasks()
+        case 3:
+            remove_task()
+        case 4:
+            mark_task()
+        case 5:
+            exit_progarm()
+        case _:
+            show_menu()
+
+def navi_menu():
+    global available_menu, current_menu
+    available_menu = []
+    nav_menu = ""
+    for menu in menues:
+        if menu is current_menu or (task.count <= 0 and (menu is 3 or menu is 4)):
+            continue
+        nav_menu += (f"[{menu}-{menues[menu]}] ")
+        available_menu.append(menu)
+    print(tabulate(([nav_menu],)))
 
 def view_tasks(style_no=22):
     if task.count <= 0:
@@ -81,14 +112,6 @@ def bound_in_range(number):
     upper_bound = len(styles) - 1
     return max(min(number, upper_bound), lower_bound)
 
-def show_menu():
-    global current_menu
-    clear_screen()
-    app_title()
-    open_current_menu()
-    navi_menu(current_menu)
-    select_menu()
-
 def add_task():
     global current_menu
     print("(Note: Press 'ctr + c' to save)")
@@ -97,13 +120,15 @@ def add_task():
     exit = False
     while not exit:
         try:
-            verify_input(input(f"{task.count + 1}: "))
-            
+            new_task = input(f"{task.count + 1}: ")
+            verify_task_input(new_task)
+            task.add(new_task.strip())
         except KeyboardInterrupt:
             current_menu = 2
             exit = True
+    show_menu()
 
-def verify_input(new_task):
+def verify_task_input(new_task):
     if not new_task.strip():
         print('\033[31m can\'t add empty task \033[0m')
         time.sleep(1)
@@ -112,8 +137,7 @@ def verify_input(new_task):
         print('\033[31m task already exist \033[0m')
         time.sleep(1)
         show_menu()
-    task.add(new_task.strip())
-
+    
 def already_in_list(new_task):
     if task.count <= 0:
         return False
@@ -126,23 +150,6 @@ def print_previous_tasks():
     if task.count > 0:
         for i in range(task.count):
             print(f"{i + 1}: {task.tasks[i][0]}")
-
-def open_current_menu():         
-    global current_menu           
-    match current_menu:
-        case 1:
-            add_task()
-            show_menu()
-        case 2:
-            view_tasks()
-        case 3:
-            remove_task()
-        case 4:
-            mark_task()
-        case 5:
-            exit_progarm()
-        case _:
-            show_menu()
 
 def mark_task():
     global current_menu, input_taken
@@ -212,8 +219,6 @@ def remove_task():
         current_menu = 2
         show_menu()
 
-
-
 def remove_by_name(text):
     global input_taken
     if not task.remove_by_name(text):
@@ -234,17 +239,6 @@ def remove_by_index(text):
         input_taken = False
         show_menu()
         #remove_task()
-
-def navi_menu(current_menu):
-    global available_menu
-    available_menu = []
-    nav_menu = ""
-    for menu in menues:
-        if menu is current_menu or (task.count <= 0 and (menu is 3 or menu is 4)):
-            continue
-        nav_menu += (f"[{menu}-{menues[menu]}] ")
-        available_menu.append(menu)
-    print(tabulate(([nav_menu],)))
 
 def clear_screen():
     if os.name == "posix": # POSIX systems (Linux, MacOS)
